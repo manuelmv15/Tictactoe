40bc7b17de7e26d197027cc70414ccff930b0267
manuelmv15 <mv23036@ues.edu.sv>
2024-08-05 13:43:06 -0600

    subida del proyecto

--- src/main/java/com/mycompany/tictactoe/Tictactoe.java
+++ src/main/java/com/mycompany/tictactoe/Tictactoe.java
@@ -11,6 +11,6 @@ package com.mycompany.tictactoe;
 public class Tictactoe {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("hola");
     }
 }
